4b04b519d3edbc210d7f58207dd8c047fd51570f
niklagutin <lagutin.na@yandex.ru>
2022-02-06 15:01:32 +0300

    Added stylelint plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import viteStylelint from '@amatlash/vite-plugin-stylelint';
 
 export default defineConfig({
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue(), eslintPlugin(), viteStylelint()],
 });
