02f5ef5e61d6025bb52c39a8fab9fe79fddb2546
niklagutin <lagutin.na@yandex.ru>
2022-02-06 15:32:38 +0300

    Added first simple rules for elsint and stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,18 @@ module.exports = {
         node: true,
     },
     extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'vue/attribute-hyphenation': [
+            'error',
+            'always',
+            {
+                ignore: ['viewBox'],
+            },
+        ],
+        'vue/no-v-html': 'off',
+    },
 };
