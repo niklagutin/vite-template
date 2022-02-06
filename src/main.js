831879fda4dc4c86035affe45ec842a7ad48a191
niklagutin <lagutin.na@yandex.ru>
2022-02-06 16:33:23 +0300

    Added normalize.css and reset.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+
+import 'normalize.css';
+import 'reset-css';
+import '@/assets/scss/common.scss';
+
 import App from './App.vue';
 
 createApp(App).mount('#app');
